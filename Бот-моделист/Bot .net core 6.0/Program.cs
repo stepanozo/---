17e97eb8720515ffafbde4d21fc10b3c11aeb98b
stepanozo <121490039+stepanozo@users.noreply.github.com>
2023-02-12 16:16:27 +0300

    Add files via upload

---
+++ Бот-моделист/Bot .net core 6.0/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+//using Telegram.Bot.Exceptions.Polling;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InputFiles;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Бот_1
+{
+
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            var client = new TelegramBotClient("6019171965:AAELlnnmqAqz0vu2RnxneFsFX3Syxu-n0zU");
+
+            client.StartReceiving(Update, Error); //этих двух методов нет
+            Console.ReadLine();
+        }
+
+        //Асинхронный метод чтобы чёта в потоках?
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            //это контент, который мы получаем
+            var message = update.Message;
+
+
+
+            if(message.Text == null && message.Document != null)
+            {
+
+                try
+                {
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, text: "Сейчас обработаем фото...", replyMarkup: Variables.DefaultKeyboard);
+
+                    var fileId = update.Message.Document.FileId;        //Id файла получаем
+                    var fileInfo = await botClient.GetFileAsync(fileId); //Создаём переменную, в которой будет вся инфа по файлу.
+                    var filePath = fileInfo.FilePath;                      //Создаём переменную, в которую кладём путь к файлу
+
+                    string photoHashName = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString())).Remove(5) + ".jpg" ; //создали крутой короткий хэш по гайду с ютуба.
+                    string destinationFilePath = @"C:\Users\чтепоноза\Desktop\Обработанные танки\" + photoHashName;
+                    await using FileStream fileStream = System.IO.File.OpenWrite(destinationFilePath); //Открываем файловый поток туда, куда мы указали, то есть на наш новый файл
+                    await botClient.DownloadFileAsync(          //и сохраняем  туда наш файл
+                        filePath: filePath,
+                        destination: fileStream
+                        );
+                    fileStream.Close();     //после чего можно и нужно закрыть поток
+                                            //пока не совсем понимаю почему путь именно так записан странно
+                    if (message.Document.FileName.Substring(message.Document.FileName.Length - 4) == ".jpg")
+                    {
+                        Process processOfEditing = Process.Start(@"C:\Users\чтепоноза\Desktop\Обработанные танки\LightColorEdit.exe", $@"""{destinationFilePath}"""); //@ нужно для использования недопустимых вещей в строке
+                       // Process[] photoshop = Process.GetProcessesByName("Photoshop");
+                        int secCount = 0;
+
+                        while (!processOfEditing.HasExited) //Ждём, пока этот процесс не завершится.
+                        {
+                         
+                            
+                            await Task.Delay(1000);
+                            secCount++;
+                            processOfEditing.Refresh();
+                            if (secCount > 10)
+                            {
+                                
+                                throw new Exception();
+                            }
+                        }
+
+                        string editedFilePath = @"C:\Users\чтепоноза\Desktop\Обработанные танки\Edited\" + photoHashName;
+
+                        await using Stream stream = System.IO.File.OpenRead(editedFilePath); //открываем снова поток на наш файл
+                        await botClient.SendDocumentAsync(message.Chat.Id, new InputOnlineFile(stream, message.Document.FileName.Replace(".jpg", " (edited).jpg"))); //тут просто указываем, с каким именем бот вернёт файл
+                        stream.Close(); //закрываем поток, потому что иначе у нас оказывается будет ошибка при попытке удалить файл.
+
+
+                        System.IO.File.Delete(destinationFilePath); //была неоднозначность, так что указали, что это именно файл системы, а не телеграмма.
+                        System.IO.File.Delete(editedFilePath); //Удаляем и старый, и обработанный файлы.
+
+                    }
+                    else
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, text: "Пожалуйста, отправьте файл в формате jpg без сжатия", replyMarkup: Variables.DefaultKeyboard);
+                    }
+                    return;
+                }
+                catch
+                {
+                    Process[] photoshop = Process.GetProcessesByName("Photoshop");
+                    foreach (Process p in photoshop)
+                        p.Kill();
+
+                    Process[] editor = Process.GetProcessesByName("LightColorEdit");
+                    foreach (Process p in editor)
+                        p.Kill();
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, text: "Что-то пошло не так, и вашу фотографию не получилось обработать. Попробуйте снова.", replyMarkup: Variables.DefaultKeyboard);
+                    return;
+                }
+                
+
+
+
+            }
+
+
+            if (message.Text != null)
+            {
+
+                //  Console.WriteLine($"{message.Chat.FirstName} | {message.Text}");
+
+                if (!Variables.usersDict.ContainsKey(message.Chat.Id)) //Есть такой юзер или нет
+                {
+                    Variables.usersDict[message.Chat.Id] = new User();
+                    Console.WriteLine($"{message.Chat.FirstName} - зарегистрирован новый пользователь");
+                    Console.WriteLine($"{message.Chat.FirstName} with id {message.Chat.Id} started the bot");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, text: "Я пробудился. Мои команды:\n\nРеплики\nФото\nПомощь\nИгра\nСтоп", replyMarkup: Variables.DefaultKeyboard);
+                    return;
+                }
+                else if (message.Text == "/start")
+                {
+                    Console.WriteLine($"{message.Chat.FirstName} with id {message.Chat.Id} started the bot");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Мои команды:\n\nРеплики\nФото\nПомощь\nИгра\nСтоп");
+                 
+                    return;
+                }
+                else
+                {
+
+                    if (message.Text.ToLower().Contains("здорова"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Здоровей видали");
+                        return;
+                    }
+
+                   
+                }
+
+            }
+            return;
+
+        }
+
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Бот-моделист/Bot .net core 6.0/Variables.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Бот_1
+{
+
+    internal class User
+    {
+        public bool phraseRequest;
+        public int gameLevel;
+        public int trueAnswers;
+        public string heroName;
+        public string heroPhrase;
+        public bool stopGameForReplicsQuestion;
+        public bool continueGameQuestion;
+        public bool restartGameQuestion;
+        public bool continueReplicsQuestion;
+        public bool stopReplicsForGameQuestion;
+
+
+
+        public User()
+        {
+            phraseRequest = false;
+            gameLevel = 0;
+            trueAnswers = 0;
+        }
+
+    }
+
+
+
+    internal static class Variables
+    {
+
+        public static ReplyKeyboardMarkup DefaultKeyboard = new(
+                            new[]
+                            {
+                                new KeyboardButton[] { "Реплики", "Фото"},
+                                new KeyboardButton[] { "Помощь", "Игра"},
+                                new KeyboardButton[] { "Стоп"}
+                            })
+        {
+            ResizeKeyboard = true
+        };
+
+        public static ReplyKeyboardMarkup YesOrNoKeyboard = new(
+                            new[]
+                            {
+                                new KeyboardButton[] { "Да"},
+                                new KeyboardButton[] { "Нет"},
+                            })
+        {
+            ResizeKeyboard = true
+        };
+
+        public static ReplyKeyboardMarkup GameAndReplicsKeyboard = new(
+                           new[]
+                           {
+                                new KeyboardButton[] { "Граф Морт", "Советник Гориус"},
+                                new KeyboardButton[] { "Великус", "Хаз'Гуул Собиратель черепов"},
+                                new KeyboardButton[] { "Болотный доктор", "Говард Альтеренас"},
+                                new KeyboardButton[] { "Стоп"}
+                           })
+        {
+            ResizeKeyboard = true
+        };
+
+        public static Dictionary<long, User> usersDict = new Dictionary<long, User>();
+
+        public static string[] gameQuestions = { "Так значит, этот рычаг откроет дверь к твоей свободе, трус?", "Натрезимы - коварный демонический народ. Я совсем немного читал о них в нашей библиотеке. Они крайне опасны.", "Эти пещеры - развалины древнего города дворфов, но сейчас его населяют только жалкие кобольды, и нам нечего бояться.", "Не подходи, сова-переросток! Поверь, пожалеешь!", "Стоп! На нас будут нападать?!", "Что? Кто это там шастает по собору посреди ночи?", "Прошу вас ещё поразмыслить над предложением.", "Поверь, Говард. Начав эту битву, ты умрёшь. Я же великодушно предлагаю тебе сложить оружие и уйти.", "Пора свершиться правосудию света!", "Каждый выживает как может, служитель света. Каждый сам за себя." };
+        public static string[] gameCorrectAnswers = { "Верно! Это он сказал Морту в пещере.", "Да, это был именно советник. Он пытался предупредить Морта об опасности, исходящей от Великуса.", "Да. Великус поразительно хорошо знает географию и историю Азерота.", "Именно. Совой-переростком позже оказался Великус...", "И снова реплика графа Морта!", "Да. Именно этой ночью советник слишком много узнал...", "Ну конечно! Не стоило графу принимать предложение Болотного Доктора...", "Да. Против Хаз'Гуула действительно сложно драться.", "И правосудие свершилось.", "Да, это был главный герой кампании" };
+        public static string[] gameWrongAnswers = { "А вот и неправда.", "А вот и нет.", "Нет, этот ответ неправильный", "Неверно.", "Не-а.", "Ну уж нет.", "Ответ неправильный", "Нет, это произнёс другой персонаж.", "Нет, это неверно.", "Нет, неправильно" };
+        public static string[] gameCorrectVariants = { "Говард Альтеренас", "Советник Гориус", "Великус", "Граф Морт", "Граф Морт", "Советник Гориус", "Болотный доктор", "Хаз'Гуул Собиратель черепов", "Говард Альтеренас", "Граф Морт" };
+    }
+
+
+
+}
